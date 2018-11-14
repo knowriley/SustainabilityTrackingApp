d8995df265295dcb9b0cb3afd747fa31ea77f2ff
q9l1b <q9l1b@ugrad.cs.ubc.ca>
2018-11-14 14:11:33 -0800

    Observer removed

--- SRC/ui/VisualMain.java
+++ SRC/ui/VisualMain.java
@@ -1,41 +1,41 @@
-//package ui;
-//
-//import model.Accomplishment.Accomplishment;
-//import model.User.User;
-//
-//import javax.swing.*;
-//import java.awt.*;
-//import java.util.ArrayList;
-//
-//public class VisualMain extends JFrame{
-//    public static final int WIDTH = 1000;
-//    public static final int HEIGHT = 700;
-//    private User user;
-//
-//    public VisualMain(){
-//        initializeGraphics();
-//    }
-//
-//    public static void main(String[] args) {
-//
-//    }
-//
-//    private void initializeGraphics(){
-//        setLayout(new BorderLayout());
-//        setMinimumSize(new Dimension(WIDTH, HEIGHT));
-//        createOptions();
-//        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        setLocationRelativeTo(null);
-//        setVisible(true);
-//    }
-//
-//
-//    private void createOptions(){
-//        JPanel options = new JPanel();
-//        options.setLayout(new GridLayout(3, 1));
-//        options.setSize(new Dimension(0, 0));
-//        add(options, BorderLayout.CENTER);
-//
-//    }
-//
-//}
+package ui;
+
+import model.Accomplishment.Accomplishment;
+import model.User.User;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
+public class VisualMain extends JFrame{
+    public static final int WIDTH = 1000;
+    public static final int HEIGHT = 700;
+    private User user;
+
+    public VisualMain(){
+        initializeGraphics();
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    private void initializeGraphics(){
+        setLayout(new BorderLayout());
+        setMinimumSize(new Dimension(WIDTH, HEIGHT));
+        createOptions();
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+
+    private void createOptions(){
+        JPanel options = new JPanel();
+        options.setLayout(new GridLayout(3, 1));
+        options.setSize(new Dimension(0, 0));
+        add(options, BorderLayout.CENTER);
+
+    }
+
+}
